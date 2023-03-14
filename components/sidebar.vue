5fd6906067acaef48c5ad53b940cd57d9ccbb7b3
Khairul Haaziq <rhycoz@yahoo.com>
2023-03-15 02:03:18 +0800

    first commit

---
+++ components/sidebar.vue
@@ -0,0 +1,6 @@
+<template>
+        <div class="w-[16rem] border-r border-[#282828] h-screen flex flex-col p-4">
+        <h1>runcit</h1>
+                <div><Icon name="lucide:home" size="2rem" />Home</div>
+        </div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+	<div class="flex dark:bg-[#131313] dark:text-white">
+		<Sidebar />
+		<slot />
+	</div>
+</template>
